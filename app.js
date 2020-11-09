b6b98af115525fb860fd436b685f8519aa066c01
Artur Akhmadullin <arturakk2015@gmail.com>
2020-11-09 06:06:44 +0300

    added popup with video

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-document.body.addEventListener('click', () => {
-  document.querySelector('video').play();
+// document.body.addEventListener('click', () => {
+//   document.querySelector('video').play();
+// })
+
+const playBtn = document.querySelector('.play-video');
+const wrapper = document.querySelector('.wrapper');
+const popup = document.querySelector('.popup');
+const close = document.querySelector('.close');
+const iFrame = document.querySelector('.popup iframe');
+
+playBtn.addEventListener('click', () => {
+  popup.classList.remove('visually-hidden');
+  wrapper.classList.add('wrapper--overlay');
+  iFrame.setAttribute('src', 'https://player.vimeo.com/video/436752564');
 })
+
+close.addEventListener('click', () => {
+  popup.classList.add('visually-hidden');
+  wrapper.classList.remove('wrapper--overlay');
+  iFrame.setAttribute('src', '');
+});
